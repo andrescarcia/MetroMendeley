cbf939c8862b353be4dacb7d8461de7984574617
André Scarcia <andre.scarcia@correo.unimet.edu.ve>
2023-03-22 17:06:55 -0400

    Interfaces Listas y comienzo del proyecto 2 :100:

---
+++ MetroMendeley/src/metromendeley/MetroMendeley.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package metromendeley;
+
+import GUI.classes.AgregarResumen;
+
+/**
+ *
+ * @author andre
+ */
+public class MetroMendeley {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AgregarResumen main = new AgregarResumen();
+        main.setVisible(true);
+    }
+    
+}
